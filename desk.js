9e9fabf20ced2cdc131294e6ece616896138b4e1
Matthew Knox <mrk45@uclive.ac.nz>
2016-11-21 13:51:51 +1300

    Remove old match version

--- desk.js
+++ desk.js
@@ -19,18 +19,6 @@ function deskctl(dir, api, event) {
 	});
 }
 
-exports.match = function(text, commandPrefix) {
-    return text === commandPrefix + 'dionsdesk' || text.startsWith(commandPrefix + 'dionsdesk ');
-};
-
-exports.help = function() {
-    return [
-		[this.commandPrefix + 'dionsdesk','Calls you an idiot.'],
-		[this.commandPrefix + 'dionsdesk up','Annoys the Dion by moving his desk up for 1 second.'],
-		[this.commandPrefix + 'dionsdesk down','Crushes the Dion by moving his desk down for 1 second.']
-	];
-};
-
 exports.run = function(api, event) {
 	var cmds = event.body.split(' ');
 	if (cmds.length === 1 || cmds.length > 2) {
